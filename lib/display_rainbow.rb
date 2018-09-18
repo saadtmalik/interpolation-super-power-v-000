f4252f5797934056fbe52f58c6f7b5d21b41947f
Saad Malik <saad.t.malik@gmail.com>
2018-09-18 17:33:58 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,10 +1,10 @@
 def #display_rainbow ("colors")
   colors = "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
-  puts= "#{color[0]}"
-  puts= "#{color[1]}"
-  puts= "#{color[2]}"
-  puts= "#{color[3]}"
-  puts= "#{color[4]}"
-  puts= "#{color[5]}"
-  puts= "#{color[6]}"
+  puts "#{color[0]}"
+  puts "#{color[1]}"
+  puts "#{color[2]}"
+  puts "#{color[3]}"
+  puts "#{color[4]}"
+  puts "#{color[5]}"
+  puts "#{color[6]}"
 end
